8361fe2653bf820ad1ca28e6ac1185f0bbb4bd4a
Dillon Donoho <scopicaudio@gmail.com>
2022-03-18 13:03:56 -0400

    sending to justin

---
+++ exercise/src/main/java/com/techelevator/Project.java
@@ -0,0 +1,11 @@
+package com.techelevator;
+
+import java.util.List;
+
+public class Project {
+    private String name;
+    private String description;
+    private String startDate;
+    private String dueDate;
+    private List<Employee> teamMembers;
+}
